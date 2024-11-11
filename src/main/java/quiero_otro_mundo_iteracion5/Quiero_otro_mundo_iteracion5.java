7550fc3e76ca015e99fc550dd52694b2d268cc35
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-11-11 04:04:01 -0300

    primer comit

---
+++ src/main/java/quiero_otro_mundo_iteracion5/Quiero_otro_mundo_iteracion5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package quiero_otro_mundo_iteracion5;
+
+/**
+ *
+ * @author Kathy
+ */
+public class Quiero_otro_mundo_iteracion5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
